b9de75d02b5a4a99c830712fd3f290cd57e654f0
Omer Yefet <omeryefet@gmail.com>
2024-03-26 15:26:11 +0200

    second bse commit

--- src/MyClass1.java
+++ src/MyClass1.java
@@ -1,5 +1,10 @@
 public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("new content");
+        System.out.println("base content");
+
+        System.out.println("base content4");
+        System.out.println("base content2");
+
     }
 }
